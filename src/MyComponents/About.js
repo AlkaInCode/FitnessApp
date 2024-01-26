48aedbfe31cc6bd3993b2db8e5b6b98dbb578403
thebrownhuman <thebrownhuman@gmail.com>
2024-01-26 17:33:49 +0530

    Checking the branch is working or not

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div>
       This is About of MyComponent
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet sequi, quaerat placeat, omnis recusandae temporibus maxime consequuntur tempora praesentium alias eligendi, error ex ipsam explicabo odio impedit assumenda officiis rem cumque vel. Voluptates, fugit earum.</p>
+      <p>Lorem ipsum, nsectetur adipisicing elit. Amet sequi, quaerat placeat, omnis recusandae temporibus maxime consequuntur tempora praesentium alias eligendi, error ex ipsam explicabo odio impedit assumenda officiis rem cumque vel. Voluptates, fugit earum.</p>
     </div>
   )
 }
